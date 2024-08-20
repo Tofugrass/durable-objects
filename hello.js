376616a5d6e0df5834696a0fbedf1e9b2af823bd
MicahDerusha <92353955+MicahDerusha@users.noreply.github.com>
2024-08-20 03:30:23 -0500

    handle set-timer

--- hello.js
+++ hello.js
@@ -4,9 +4,19 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from gtihub!\n');
+
+  if(req.url.includes('/set-timer')){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Called set-timer!\n');
+    console.log(req.url)
+  }
+  else{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World from gtihub!\n');
+
+  }
 });
 
 server.listen(port, hostname, () => {
